af37d8fba3027b995b2eeda8aaa2dbff68326de8
Chenyang Feng <CFeng@walmartlabs.com>
2016-07-11 14:50:22 -0700

    initial

---
+++ SettingViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingViewController.swift
+//  tippy
+//
+//  Created by Chenyang Feng on 7/10/16.
+//  Copyright © 2016 Chenyang Feng. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+
+    @IBAction func change2(sender: AnyObject) {
+        defaults.setDouble(0.7, forKey: "last_tip")
+        defaults.synchronize()
+    }
+    
+    @IBAction func change1(sender: AnyObject) {
+        defaults.setDouble(0.9, forKey: "last_tip")
+        defaults.synchronize()
+    }
+
+}
